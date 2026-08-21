db32b119eb047bfb9126d3777a6ac2d210b306b5
Nathalia Suzin <natsuzin22@gmail.com>
2026-08-21 15:56:58 -0300

    test: interative mode file updated

--- docs/manual-tests/interactive-mode-pixel-mover.s
+++ docs/manual-tests/interactive-mode-pixel-mover.s
@@ -8,6 +8,18 @@ main:
     # t3 = keyboard register address (0xFF010000), fixed for the whole run
     li t3, -16711680
 
+    # t0 = framebuffer base address (0xFF000000)
+    li t0, -16777216
+
+    # draw the initial pixel before waiting for any key, so something
+    # is visible immediately after Assemble/Run
+    slli t2, s2, 5
+    add  t2, t2, s1
+    slli t2, t2, 2
+    add  t2, t2, t0
+    li   t1, -1
+    sw   t1, 0(t2)
+
 loop:
     # t4 = last key code written by the panel; 0 means no pending key
     lw t4, 0(t3)
@@ -38,16 +50,38 @@ loop:
 
 move_left:
     addi s1, s1, -1
-    j draw
+    j wrap_check
 move_right:
     addi s1, s1, 1
-    j draw
+    j wrap_check
 move_up:
     addi s2, s2, -1
-    j draw
+    j wrap_check
 move_down:
     addi s2, s2, 1
-    j draw
+    j wrap_check
+
+wrap_check:
+    # wrap x: if s1 went below 0, jump to the right edge (31);
+    # if it went past 31, jump to the left edge (0)
+    bgez s1, wrap_check_x_high
+    li   s1, 31
+    j    wrap_check_y
+wrap_check_x_high:
+    li   t5, 31
+    ble  s1, t5, wrap_check_y
+    li   s1, 0
+
+wrap_check_y:
+    # wrap y: if s2 went below 0, jump to the bottom edge (17);
+    # if it went past 17, jump to the top edge (0)
+    bgez s2, wrap_check_y_high
+    li   s2, 17
+    j    draw
+wrap_check_y_high:
+    li   t5, 17
+    ble  s2, t5, draw
+    li   s2, 0
 
 draw:
     # recompute t2 for the (possibly updated) position and light it up
